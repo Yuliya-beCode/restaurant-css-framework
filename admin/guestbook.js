02c6a6a95c111c95fbd6e0e59ac108fafeaa4fee
Yuliya Bochkovskaya <yuliabochkovskaya@gmail.com>
2021-03-26 14:17:29 +0100

    api deleted

--- admin/guestbook.js
+++ admin/guestbook.js
@@ -25,13 +25,13 @@ function execute() {
 
 gapi.load("client:auth2", function () {
   gapi.auth2.init({
-    client_id: "997997438942-lpgucmkg13p8sr4l6g0tjrpt2akqk30a.apps.googleusercontent.com"
+    client_id: ":-)"
   });
 });
 
 
 function loadClient() {
-  gapi.client.setApiKey("AIzaSyD0v8HIYDAydw43WJqFoH_vjdt4lBhq54g");
+  gapi.client.setApiKey(":-)");
   return gapi.client.load("https://sheets.googleapis.com/$discovery/rest?version=v4")
     .then(function () {
         execute().then(({
